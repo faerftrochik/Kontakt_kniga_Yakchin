22057e1f544c68ceee316ffc0d456c5fdbc01e1e
Souls Rider <faerftrochik@gmail.com>
2026-04-11 16:46:07 +0300

    Napisal osnovi

--- app/src/main/java/com/example/kontaktnay_kniga_yakchin/MainActivity.kt
+++ app/src/main/java/com/example/kontaktnay_kniga_yakchin/MainActivity.kt
@@ -15,6 +15,54 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            fun callPhoneNumber(phoneNumber: String) {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            }
+
+            callPhoneNumber("+74951234567")
+
+            fun sendEmail(address: String, subject: String, body: String) {
+                val intent = Intent(Intent.ACTION_SENDO).apply {
+                    date = Uri.parse("mailto:")
+
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
+                    putExtra(Intent.EXTRA_SUBJECT, subject)
+                    putExtra(Intent.EXTRA_TEXT, body)
+                }
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            }
+
+            sendEmail("contact@example.com", "Обращение", "Добрый день, \n")
+
+            fun showOnMap(latitude: Double, longitude: Double, label: String) {
+                val geoUri = Uri.parse("geo:0,0?q=$latitude,$longitude($label)")
+                val intent = Intent(Intent.ACTIVE_VIEW, geoUri)
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            }
+
+            showOnMap(60.0237, 30.2289, "Наш офис")
+
+
+            fun shareText(text: String) {
+                val sendIntent = Intent(Intent.ACTION_SEND).apply {
+                    type = "text/plain"
+                    putExtra(Intent.EXTRA_TEXT, text)
+                }
+
+                val chooser = Intent.createChooser(sendIntent, "Поделиться через...")
+                startActivity(chooser)
+            }
+
+            shareText("Контакт: +7 (495) 123-45-67, contact@example.com")
+
         }
     }
 }
