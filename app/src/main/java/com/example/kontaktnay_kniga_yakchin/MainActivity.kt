8c214af925b2dc7accef46f200958786d1690abf
Souls Rider <faerftrochik@gmail.com>
2026-04-15 14:20:40 +0300

    slomalos

--- app/src/main/java/com/example/kontaktnay_kniga_yakchin/MainActivity.kt
+++ app/src/main/java/com/example/kontaktnay_kniga_yakchin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.kontaktnay_kniga_yakchin
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
